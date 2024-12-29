747df2292e9480e42d8a888b16693e0be5ed050d
skinpain4l <sydomn90872@gmail.com>
2024-12-28 23:31:26 -0500

    implement the file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+//import type { Metadata } from 'next'
+//import { Geist, Geist_Mono } from "next/font/google";
+//import './globals.css'
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return <main>SIDEBAR{children}</main>
+}
